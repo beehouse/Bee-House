1dea94df3ad3baaf0006e9271ca12011f9051d5c
Clayton Albachten <clay.reed.a@gmail.com>
2015-02-15 19:52:46 -0500

    Super awesome button to return a loan

--- app/assets/javascripts/views/patrons/patron_item_loan.js
+++ app/assets/javascripts/views/patrons/patron_item_loan.js
@@ -1,13 +1,35 @@
 BeeHouse.Views.PatronItemLoan = Backbone.View.extend(
   {
     template: JST['loans/patron_loan'],
-    initialize: function(){
+    events: {
+      'click .return-loan': 'returnLoan'
+    },
+    returnLoan: function(){
+      var that = this, 
+        loanId = this.model.id,
+   currentUser = BeeHouse.session.get('currentUser');
+      var loan = new BHLoan({id: loanId});
 
+      var loanPatronId = parseInt(this.model.patron_id),
+      currentUserId = parseInt(currentUser.get('id'));
+
+      loan.destroy()
+        .done(function(){
+
+          if (currentUserId === loanPatronId) {
+            BHEvents.trigger('reloadCurrentUser');
+          }
+
+          that.close();
+        });
     },
     render: function(){
+      var currentUser = BeeHouse.session.get('currentUser');
+      var admin = currentUser.isAdmin();
       $(this.el).html(this.template(
         {
-          loan: this.model
+          loan: this.model,
+          admin: admin
         }
       ));
 
