5ea74aef7870c65eaa9249a6bda71f1bae30ebf9
Clayton Albachten <clay.reed.a@gmail.com>
2015-02-22 19:27:26 -0500

    Half of stripe (is better than no stripe)

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ source 'https://rubygems.org'
 gem 'rails', '4.2'
 # Use sqlite3 as the database for Active Record
 
+gem 'stripe'
+
 group :development, :test do
   gem 'sqlite3' 
 end 
--- app/assets/javascripts/views/patrons/payments.js
+++ app/assets/javascripts/views/patrons/payments.js
@@ -1,12 +1,39 @@
 BeeHouse.Views.PaymentsView = Backbone.View.extend(
   {
     template: JST['patrons/payments'],
-    initialize: function(){},
-    render: function(){
-      $(this.el).html(this.template());
+    events: {
+      'submit form': 'processCard'
+    },
+    checkWithStripe: function(status, response){
+      console.log(status);
+      if (status == 200) {
+        console.log(response.id);
+      } else {
+        console.log(response.error.message);
+      }
 
-      this.$('.main__nav').append(new BeeHouseNav().render().el);
+    },
+    processCard: function(e){
+      e.preventDefault();
+      console.log('You submitted a form!');
+      var card = {
+        number: this.$('#payments__number').val(),
+        cvc: $('#payments__cvs').val(),
+        expMonth: $('#payments__exp-month').val(),
+        expYear: this.$('#payments__exp-year').val()
+      };
+      console.log(JSON.stringify(card));
+      Stripe.createToken(card, this.checkWithStripe);
+    },
+    initialize: function(){
+      Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
+    },
+    render: function(){
       
+      $(this.el).html(this.template());
+ 
+      this.$('.main__nav').append(new BeeHouseNav().render().el);
+
       return this; 
     }
   }
