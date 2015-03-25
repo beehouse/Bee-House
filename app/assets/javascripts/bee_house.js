4f73308ad58a47bb795ddcf8b00b336774d189a4
Clayton Albachten <clay.reed.a@gmail.com>
2015-03-25 18:51:38 -0400

    Titles will also show More Info

--- app/assets/javascripts/bee_house.js
+++ app/assets/javascripts/bee_house.js
@@ -19,8 +19,7 @@ _.extend(BHEvents, Backbone.Events);
 
 $(document).ready(function(){
   CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
-  BeeHouse.session = new BeeHouseSession(); // look for auth token, 
-                                            // look for user id 
+  BeeHouse.session = new BeeHouseSession(); 
   BeeHouse.session.getCurrentUser(function(){
     BeeHouse.begin();
   }); 
