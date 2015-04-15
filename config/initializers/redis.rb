9c342d065e457e8279592e4c2bacdcf72d363a50
Clayton Albachten <clay.reed.a@gmail.com>
2015-04-15 19:21:44 -0400

    Try REDIS_URL

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-uri = URI.parse(ENV["REDIS_PROVIDER"] || 'redis://127.0.0.1:6379')
-REDIS = Redis.new(:url => ENV['REDIS_PROVIDER'] || 'redis://127.0.0.1:6379')
+uri = URI.parse(ENV["REDIS_URL"] || 'redis://127.0.0.1:6379')
+REDIS = Redis.new(:url => ENV['REDIS_URL'] || 'redis://127.0.0.1:6379')
