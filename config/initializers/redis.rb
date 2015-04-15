9e99e9ddbf6a1f62e837805de1a3689b35971322
Clayton Albachten <clay.reed.a@gmail.com>
2015-04-15 19:32:05 -0400

    Try REDISTOGO_URL

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-uri = URI.parse(ENV["REDIS_URL"] || 'redis://127.0.0.1:6379')
-REDIS = Redis.new(:url => ENV['REDIS_URL'] || 'redis://127.0.0.1:6379')
+uri = URI.parse(ENV['REDISTOGO_URL'] || 'redis://127.0.0.1:6379')
+REDIS = Redis.new(:url => ENV['REDISTOGO_URL'] || 'redis://127.0.0.1:6379')
